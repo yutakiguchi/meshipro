0c6ca5069185a6307a968982f7fc5c1e8ef4187a
yutakiguchi <y--k080816@docomo.ne.jp>
2021-06-24 07:18:49 +0000

    raty.js改善2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -68,7 +68,15 @@ $(function(){
 });
 
 
-  
+  $('.average-review-rating-index').raty({
+      readOnly: true,
+      starOn: "<%= asset_path('star-on.png') %>",
+      starOff: "<%= asset_path('star-off.png') %>",
+      starHalf: "<%= asset_path('star-half.png') %>",
+        score: function() {
+        return $(this).attr('data-score')
+        }
+    });
 
 
 
